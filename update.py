aeb35bb0654b3e5eb8db336934ee5dbce8860b09
rtromp4 <156442986+rtromp4@users.noreply.github.com>
2024-01-15 14:41:58 +0600

    Update update.py

--- update.py
+++ update.py
@@ -56,7 +56,7 @@ if len(UPSTREAM_REPO) == 0:
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
-    UPSTREAM_BRANCH = 'beta'
+    UPSTREAM_BRANCH = 'main'
 
 if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
